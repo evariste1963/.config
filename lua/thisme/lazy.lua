65913d48b044b41fe202315bc00fc9239605783b
thisme <evariste@hotmail.co.uk>
2024-12-12 20:55:16 +0000

    mason-lsp
    only lua-ls actually works!!.. atthe moment!

--- lua/thisme/lazy.lua
+++ lua/thisme/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({  import = "thisme.plugins"  }, {
+require("lazy").setup({ {  import = "thisme.plugins"  }, { import = "thisme.plugins.lsp" }}, {
   checker = {
     enabled = true,
     notify = false,
