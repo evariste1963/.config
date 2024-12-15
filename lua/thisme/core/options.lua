afb9d7110517131cca6682ced19333eb81f59a22
thisme <evariste@hotmail.co.uk>
2024-12-15 17:31:00 +0000

    few config tweaks
    neovim from Vhyrro
    https://www.youtube.com/watch?v=N-RFCfs6rxI&list=PLx2ksyallYzW4WNYHD9xOFrPRYGlntAft&index=1

--- lua/thisme/core/options.lua
+++ lua/thisme/core/options.lua
@@ -29,11 +29,14 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-
+--opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard = "unnamedplus"
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+--scroll
+vim.opt.scrolloff = 999 -- keeps cursor in middle of screen when scrolling long files
