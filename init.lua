713ea4beffc220c012a5127d3eed0342ccece103
thisme <evariste@hotmail.co.uk>
2024-12-15 10:23:14 +0000

    internal Terminal
    TJ's internal :term custom config

--- init.lua
+++ init.lua
@@ -1,7 +1,22 @@
 require("thisme.core")
-require("thisme.lazy") 
+require("thisme.lazy")
 
 vim.opt.clipboard = "unnamedplus"
 
 -- this is just a test to see if the ssh connection with github works in lazygit now
 -- this is another test from work
+--
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 10)
+end)
