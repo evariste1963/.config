44c6fd517d12572d9cf1477964b465a7fada9ef4
evariste1963 <evariste@hotmail.co.uk>
2024-12-29 08:37:39 +0000

    imagmagick.lua
    hererocks line added if 5.1 is not installed globally

--- lua/thisme/plugins/imagemagick.lua
+++ lua/thisme/plugins/imagemagick.lua
@@ -2,6 +2,10 @@ return {
   "3rd/image.nvim",
   opts = {},
 
+  rocks = {
+    hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
+  },
+
   config = function()
     require("image").setup({
       backend = "kitty",
